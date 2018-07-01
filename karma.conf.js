aaaa488a9ee7bb85cf1949ad780024fd3d3dc30b
George Griffiths <georgegriffiths@live.com>
2018-07-01 22:14:54 +0100

    use chrome headless

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome', 'Firefox'],
+        browsers: ['ChromeHeadless', 'Firefox'],
 
 
         // Continuous Integration mode
