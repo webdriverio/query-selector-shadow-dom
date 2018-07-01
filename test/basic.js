d4832d64df1e8b9552821742ec243d42c8918481
George Griffiths <georgegriffiths@live.com>
2018-07-01 23:01:57 +0100

    only use chrome for local dev

--- test/basic.js
+++ test/basic.js
@@ -5,11 +5,30 @@ import { querySelectorAllDeep, querySelectorDeep } from '../src/querySelectorDee
 
 describe("Basic Suite", function() {
 
+    let parent;
+
+    beforeEach(() => {
+        parent = document.createElement('div');
+        document.body.appendChild(parent);
+    });
+
+    afterEach(() => {
+        parent.remove();
+    });
+
     it("exports querySelectorAllDeep function", function() {
         expect(querySelectorAllDeep).toEqual(jasmine.any(Function));
     });
 
     it("exports querySelectorDeep function", function() {
         expect(querySelectorAllDeep).toEqual(jasmine.any(Function));
+    });
+
+    it('can access an element in the light dom', function() {
+
+    });
+
+    it('can access an element in the shadow dom', function() {
+
     });
 });
