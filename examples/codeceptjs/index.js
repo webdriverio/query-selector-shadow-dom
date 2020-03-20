75257fb8b0d4139d8749192dfd6f1ae02f1d9319
George Griffiths <george.griffiths@ibm.com>
2020-03-20 18:24:45 +0000

    investigate codeceptjs and shadow dom

---
+++ examples/codeceptjs/index.js
@@ -0,0 +1,32 @@
+const { selectorEngine } = require("../playwright");
+const supportedHelpers = [
+    'Playwright'
+]
+const playwright = require('playwright');
+
+module.exports = function(config) {
+    const container = codeceptjs.container;
+    const event = codeceptjs.event;
+    const helpers = container.helpers()
+    let helperName
+    for (helperName of supportedHelpers) {
+        if (Object.keys(helpers).indexOf(helperName) > -1) {
+          helper = helpers[helperName];
+        }
+    }
+    if (!helper) {
+        throw new Error(`Shadow dom plugin only supports: ${supportedHelpers.join(',')}`)
+    }
+    if (!config) {
+        config = {}
+    }
+    if (!config.locator) {
+        config.locator = "shadow"
+    }
+
+    event.dispatcher.on(event.suite.before, async () => {
+        if(helperName === "Playwright") {
+            await playwright.selectors.register(selectorEngine, { name: config.locator })
+        }
+    });
+}
